d120e9b1fdf6882c80ef424a04b67c74fee2b1b7
Austin-Vincelli-Evans <unity7591@gmail.com>
2021-02-23 14:46:57 -0700

    aligning and sizing hero content

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,11 +4,9 @@ import { Container } from 'react-bootstrap'
 
 const Button = () => {
   return (
-    <div>
-      <Container>
-        <button className="button__donate">Donate Today</button>
-      </Container>
-    </div>
+    <>
+      <button className="button__donate">Donate Today</button>
+    </>
   )
 }
 
