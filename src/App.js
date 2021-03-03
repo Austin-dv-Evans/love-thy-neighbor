02d8b0599ccd6b09e871e3208d4b0703d9ff12df
Austin-Vincelli-Evans <unity7591@gmail.com>
2021-03-03 13:27:59 -0700

    header restyle

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import '../src/styles/Css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingPage />
+      <About />
     </div>
   );
 }
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,6 +11,9 @@ const LandingPage = () => {
                 <Container className="landing__container">
                     <h1 className='landing__jumbotron-heading1'>Love Thy Neighbor</h1>
                     <h6 className='landing__jumbotron-heading2'>
+                    Helping Those In Need
+                    </h6>
+                    <h6 className='landing__jumbotron-heading2'>
                     Together we can create hope where hope has been lost
                     </h6>
                     <Button />
