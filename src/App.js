9996fa94caef1ca3adb8defed060e81f08ad9f31
Hawleywoo <alhawley21@gmail.com>
2021-03-04 22:44:56 -0700

    Hero slider and tranistion

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import '../src/styles/Css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
-import Instagram from './components/Instagram'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingPage />
-      <Instagram /> 
     </div>
   );
 }
