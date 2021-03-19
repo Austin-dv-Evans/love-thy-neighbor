73d30bcb3c7559d17235f5dae293871d6c3a0370
Austin-Vincelli-Evans <unity7591@gmail.com>
2021-03-19 17:32:12 -0600

    responsive beginings

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,9 +7,9 @@ import HeroSlider from '../components/HeroSlider'
 const LandingPage = () => {
     return (
       <>
-      <HeroSlider/>
       <div className="landing__jumbotron">
           <img src={fullName} alt="full name logo" className="landing__nameLogo"/>
+          <HeroSlider/>
         <div className="landing__container">
           <h6 className="landing__jumbotron-heading2">Helping Those In Need</h6><br/>
           <h6 className="landing__jumbotron-heading2">
