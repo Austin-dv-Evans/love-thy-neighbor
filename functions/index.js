f12472a513d21fd9fd2015a9984ea182ac6af662
Hawleywoo <alhawley21@gmail.com>
2021-04-30 20:56:17 -0600

    Payment Intent

--- functions/index.js
+++ functions/index.js
@@ -5,29 +5,15 @@ const functions = require("firebase-functions");
 //
 exports.createStripeCheckout = functions.https.onRequest(async (request, response) =>{
   const stripe = require("stripe")(functions.config().stripe.secret_key);
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    mode: "payment",
-    success_url: 'http://localhost:4200/success',
-    cancel_url: 'http://localhost:4200/cancel',
-    line_items: [
-        {
-            quantity: 1,
-            price_data: {
-                currency: 'usd',
-                unit_amount: (100) * 100,
-                product_data: {
-                    name: "New Camera",
-                },
-            },
-        },
-    ]
+  const { amount } = request.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: 'usd'
   });
 
-  return {
-      id: session.id,
-  }
+  response.send({
+      clientSecret: paymentIntent.client_secret
+  })
 });
  
 //   app.post('', async (req, res) => {
