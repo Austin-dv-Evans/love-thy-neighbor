575c8bbd557a3ad66dd01da7531e0387f88def2e
Hawleywoo <alhawley21@gmail.com>
2021-05-14 15:01:37 -0600

    Success and fails handlers

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -103,80 +103,87 @@ const PaymentForm = ({ amount }) => {
       })
       .then(({ paymentMethod }) => {
         console.log('p method', paymentMethod)
-        
+
         return stripe.confirmCardPayment(secret, {
           receipt_email: paymentDetails.email,
           payment_method: paymentMethod.id
-        }).then( payload  => {
+        }).then(payload => {
           console.log(payload)
-          if (error) {
-            console.log(error)
-            setError(`Payment Failed ${error.message}`)
+          if (payload.error) {
+            console.log(payload.errorerror)
+            setError(`Payment Failed ${payload.error.message}`)
             setProcessing(false)
           } else {
             setError(null)
             setProcessing(false)
             setSucceeded(true)
           }
-          }).catch(error => console.log(error))
-        })
-        .catch(err => setError(err))
-        
-        // const paymentMethodRes = await stripe.createPaymentMethod({
+        }).catch(error => setError(error))
+      })
+      .catch(err => setError(err))
+
+    // const paymentMethodRes = await stripe.createPaymentMethod({
     //   type: "card",
     //   card: cardElement,
     //   billing_details: {
-      //     name: paymentDetails.name
-      //   },
-      // });
-      
+    //     name: paymentDetails.name
+    //   },
+    // });
+
     // console.log(paymentMethodRes)
     // const payload = await stripe.confirmCardPayment(clientSecret, {
     //   receipt_email: paymentDetails.email,
     //   payment_method: {
-      //     type: "card",
+    //     type: "card",
     //     card: cardElement,
     //     billing_details: {
     //       name: paymentDetails.name
     //   },}
     // })
-    
+
     // console.log(payl
   }
-  
+
   console.log(clientSecret)
   return (
     <div className="payment-form">
-      <form className="payment-form__form" onSubmit={handleSubmit}>
-        <input name="name" value={paymentDetails.name} placeholder="Name" onChange={handleInfoFormChange} />
-        <input name="email" value={paymentDetails.email} placeholder="Email" onChange={handleInfoFormChange} />
-        <input name="line1" value={address.line1} placeholder="Address" onChange={handleAddressFormChange} />
-        <input name="city" value={address.city} placeholder="City" onChange={handleAddressFormChange} />
-        <input name="state" value={address.state} placeholder="State" onChange={handleAddressFormChange} />
-        <input name="country" value={address.country} placeholder="Country" onChange={handleAddressFormChange} />
-        <input name="postal_code" value={address.postal_code} placeholder="Postal Code" onChange={handleAddressFormChange} />
-        <CardElement
-          options={{
-            style: {
-              base: {
-                color: '#424770',
-                '::placeholder': {
-                  color: '#aab7c4',
+      { succeeded ?  
+        (<div>
+          <h3>Payment Successful,  Thank You!</h3>
+          <p>You will receive and email reciept.</p>
+        </div>)
+      :
+        <form className="payment-form__form" onSubmit={handleSubmit}>
+          <input name="name" value={paymentDetails.name} placeholder="Name" onChange={handleInfoFormChange} />
+          <input name="email" value={paymentDetails.email} placeholder="Email" onChange={handleInfoFormChange} />
+          <input name="line1" value={address.line1} placeholder="Address" onChange={handleAddressFormChange} />
+          <input name="city" value={address.city} placeholder="City" onChange={handleAddressFormChange} />
+          <input name="state" value={address.state} placeholder="State" onChange={handleAddressFormChange} />
+          <input name="country" value={address.country} placeholder="Country" onChange={handleAddressFormChange} />
+          <input name="postal_code" value={address.postal_code} placeholder="Postal Code" onChange={handleAddressFormChange} />
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  color: '#424770',
+                  '::placeholder': {
+                    color: '#aab7c4',
+                  },
+                },
+                invalid: {
+                  color: '#9e2146',
                 },
               },
-              invalid: {
-                color: '#9e2146',
-              },
-            },
-          }} />
-        <button
-          className="payment-form__button"
-          disabled={processing || disabled || processing}
-        >
-          <span>{processing ? <span> processing </span> : 'Pay Now'}</span>
-        </button>
-        <h3>{error ? `Error ${error}` : ''}</h3>
-      </form>
+            }} />
+          <button
+            className="payment-form__button"
+            disabled={processing || disabled || processing}
+          >
+            <span>{processing ? <span> processing </span> : 'Pay Now'}</span>
+          </button>
+        </form>
+      }
+      <h3>{error ? `Error ${error}` : ''}</h3>
     </div>
   )
 }
